fdc46e09dcc428dcbdca2f5236d3fbc31586eff6
Miri Stoner <irimsirrom@gmail.com>
2017-06-06 17:11:06 +0300

    adding

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// module.exports.getDatabase = function(){
+// }
+
+module.exports = {
+  getDatabase : function(){
+    return 'mongodb://irimsirrom:Mirilivesat167@ds133290.mlab.com:33290/testing';
+  },
+  getSecret: function(){
+    return 'miristoner';
+  }
+}

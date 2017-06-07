641ae2fea61e344d23bc1cd686b05091edecd9c9
Miri Stoner <irimsirrom@gmail.com>
2017-06-07 11:39:47 +0300

    Connecting to backend - Register User

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -10,6 +11,18 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+
+import{ValidateService} from './services/validate.service';
+import{AuthService} from './services/auth.service';
+import{FlashMessagesModule} from 'angular2-flash-messages';
+
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'dashoard', component: DashboardComponent},
+  {path:'profile', component: ProfileComponent}
+]
 
 @NgModule({
   declarations: [
@@ -24,9 +37,11 @@ import { ProfileComponent } from './components/profile/profile.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [ValidateService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
